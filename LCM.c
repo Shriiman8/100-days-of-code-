cfe9ea5f55a20a4793501ff7dbb9175891f84797
Shriman Nautiyal <shrimannautiyal144@gmail.com>
2026-08-28 22:10:35 +0530

    thirty

---
+++ LCM.c
@@ -0,0 +1,33 @@
+//Q37: Write a program to find the LCM of two numbers.
+
+/*
+Sample Test Cases:
+Input 1:
+4 5
+Output 1:
+20
+
+Input 2:
+7 3
+Output 2:
+21
+
+*/
+#include <stdio.h>  
+int main ()
+{
+    int a,b,lcm;
+    printf("Enter two numbers : \n");
+    scanf("%d %d" , &a , &b);
+
+    for(int i = 1; i<= a*b; i++)
+    {
+        if(i%a==0 && i%b==0)
+        {
+            lcm = i;
+            break;
+        }
+    }
+    printf("LCM of %d and %d is = %d " ,a ,b ,lcm );
+    return 0;
+}
---
+++ sumofdigits.c
@@ -0,0 +1,30 @@
+//Q38: Write a program to find the sum of digits of a number.
+
+/*
+Sample Test Cases:
+Input 1:
+123
+Output 1:
+6
+
+Input 2:
+999
+Output 2:
+27
+
+*/
+#include <stdio.h>
+int main ()
+{
+    int n,sum=0, digits;
+    printf("Enter a number :");
+    scanf("%d", &n);
+    while(n!=0)
+    {
+        digits  = n%10;
+        sum = sum + digits ;
+        n = n/10;
+    }
+    printf("Sum of digits of %d = %d" ,n ,sum);
+    return 0;
+}
